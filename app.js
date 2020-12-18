50f61855ef9e350f2141c35e96ef7e8e088181b1
Olivia Lewis <ollybaby12@gmail.com>
2020-12-18 18:00:32 -0500

    added renderStartPage function

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      image: <img src="images/capheus.jpg"></img>,
+      image: "images/capheus2.jpg",
       question: 'Where does Capheus "Van Damn" Onyango live?',
       answers: [
         'Nairobi, Kenya',
@@ -16,7 +16,7 @@ const store = {
       correctAnswer: 'Nairobi, Kenya'
     },
     {
-      image: URL(images/capheus.jpg),
+      image: "images/sun.jpg",
       question: 'Sun Bak goes to prison to protect her ______?',
       answers: [
         'mother',
@@ -27,7 +27,7 @@ const store = {
       correctAnswer: 'brother'
     },
     {
-      image: images/capheus.jpg,
+      image: "images/wolfgang2.png",
       question: 'Who does Wolfgang Bogdanow fall in love with?',
       answers: [
         'Sun Bak',
@@ -38,6 +38,7 @@ const store = {
       correctAnswer: 'Kala Dandekar'
     },
     {
+      image: "images/kala.jpg",
       question: "What is Kala Dandekar's profession?",
       answers: [
         'teacher',
@@ -48,6 +49,7 @@ const store = {
       correctAnswer: 'pharmacist'
     },
     {
+      image: "images/lito.jpg",
       question: 'What is Lito Rodriguez hiding from his fans/the public?',
       answers: [
         'a criminal past',
@@ -58,6 +60,7 @@ const store = {
       correctAnswer: 'his sexual orientation'
     },
     {
+      image: "images/nomi1.jpg",
       question: "Which of her family members does Nomi Marks have the most conflict with?",
       answers: [
         'her mother',
@@ -68,6 +71,7 @@ const store = {
       correctAnswer: 'her mother'
     },
     {
+      image: "images/will.jpg",
       question: "Who does Will Gorski have the deepest connection with in his clusters?",
       answers: [
         'Lito Rodriguez',
@@ -78,6 +82,7 @@ const store = {
       correctAnswer: 'Riley Gunnarsdotir'
     },
     {
+      image: "images/riley.jpg",
       question: "Where is Riley Gunnarsdotir from?",
       answers: [
         'London',
@@ -93,7 +98,6 @@ const store = {
   score: 0
 };
 
-console.log("word");
 
 /**
  * 
@@ -114,6 +118,18 @@ console.log("word");
 
 // These functions return HTML templates
 
+var startPage = `
+<div class="noBorder">
+<img src="images/sense8.jpg">
+<div class="container">
+<h2>See how well you know the characters from Sense8.<h2>
+<button class="push-start"><span class="button-label">Start Quiz</span>
+</button></div></div>
+`
+function renderStartPage(){
+  console.log('`renderStartPage` ran')
+  $("main").append(startPage);
+}
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
@@ -121,3 +137,11 @@ console.log("word");
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+function handleQuizApp(){
+  renderStartPage();
+  //handleStartQuiz();
+  //handleNextQuestion();
+  //handleRestartQuiz();
+}
+
+$(handleQuizApp)
